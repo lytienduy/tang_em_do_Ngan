b0fdafcc09cf9c450036403a5409823188e7e9be
lytienduy <133567310+lytienduy@users.noreply.github.com>
2023-12-14 14:51:04 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Ngân ưiii 💘!',
+    introDesc: `Em có phải Christmas không?
+    Vì anh muốn Merry em 💘.`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 8 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh 💘',
+    reply: 'Yêu thì yêu mà không yêu thì yêu ❤️❤️❤️',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 8h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://lytienduy.github.io/ranh_rieng_cho_Ngan_iu/' 
+}
